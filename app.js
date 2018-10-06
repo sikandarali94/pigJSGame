036958bfde632a1199f44f62263229a5b653a79e
Sikandar Ali <sikandarali94@hotmail.com>
2018-10-07 09:56:13 +1100

    dice roll adds to round scores

--- app.js
+++ app.js
@@ -11,13 +11,15 @@ GAME RULES:
 
 /* Initialize game score variables.
  */
-let roundScore = [];
-let globalScore = [];
+let roundScore = [0,0]; // Initialize round scores for both players starting from 0.
+let globalScore = [0,0]; // Initialize global scores for both players starting from 0.
 let imgHidden = false; // false indicates dice image is shown; true indicates dice image is hidden.
+let playerTurn = 0; // 0 indicates player 0 is playing; 1 indicates player 1 is playing.
 
 const currentScores = document.getElementsByClassName('player-current-score'); // Select the HTML elements that have class of '.player-current-score'.
 const roundScores = document.getElementsByClassName('player-score'); // Select the HTML elements that have class of '.player-score'.
 const dice = document.getElementsByClassName('dice')[0]; // Select the HTML img element that shows the dice.
+const roundScorePlayer = [document.getElementById('score-0'), document.getElementById('score-1')]; // Select the HTML elements that show players' round scores.
 
 /* Define function that resets the game.
  */
@@ -37,10 +39,35 @@ function resetGame() {
 //Reset the game.
 resetGame();
 
+/* Function outputs random number between 1 and 6.
+ */
+function generateRandomDiceNumber() {
+    return Math.floor((Math.random() * 6) + 1); // Generate random number between 1 and 6.
+}
+
+/* This function changer the round score depending on dice roll.
+ */
+function alterRoundScore(diceNumber) {
+    if (diceNumber === 1) {
+        roundScore[playerTurn] = 0; // Reset the player round score if dice roll is 1.
+        roundScorePlayer[playerTurn].textContent = '0'; // Show player round score has been reset.
+        /* Change player turn.
+         */
+        if (playerTurn === 0) {
+            playerTurn++;
+        } else {
+            playerTurn--;
+        }
+    } else {
+        roundScore[playerTurn] += diceNumber; // Add dice roll to player round score.
+        roundScorePlayer[playerTurn].textContent = roundScore[playerTurn]; // Show total round score after dice roll.
+    }
+}
+
 /* Generate random dice roll.
  */
 function generateDice() {
-    const randomDiceNumber = Math.floor((Math.random() * 6) + 1); // Generate random number between 1 and 6.
+    const randomDiceNumber = generateRandomDiceNumber(); // Store random number between 1 and 6 into randomDiceNumber variable.
     const srcPath = 'dice-' + randomDiceNumber + '.png'; // Create path for image source.
 
     if (imgHidden) { // If dice is hidden.
@@ -48,8 +75,9 @@ function generateDice() {
         imgHidden = false; // Indicate dice is shown.
     }
 
-    dice.setAttribute('src', srcPath);
+    dice.setAttribute('src', srcPath); // Set src path of dice img element.
 
+    alterRoundScore(randomDiceNumber); // Call function that changes the round score depending on dice roll.
 }
 
 /* Add click event listener on button with class 'btn-roll'.
