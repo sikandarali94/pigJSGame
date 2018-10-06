909d2fa2356fc8ab149f23c2d19687ae0fda962d
Sikandar Ali <sikandarali94@hotmail.com>
2018-10-07 08:57:52 +1100

    generate dice roll by randomly showing dice image corresponding between 1 and 6

--- app.js
+++ app.js
@@ -13,13 +13,15 @@ GAME RULES:
  */
 let roundScore = [];
 let globalScore = [];
+let imgHidden = false; // false indicates dice image is shown; true indicates dice image is hidden.
+
+const currentScores = document.getElementsByClassName('player-current-score'); // Select the HTML elements that have class of '.player-current-score'.
+const roundScores = document.getElementsByClassName('player-score'); // Select the HTML elements that have class of '.player-score'.
+const dice = document.getElementsByClassName('dice')[0]; // Select the HTML img element that shows the dice.
 
 /* Define function that resets the game.
  */
 function resetGame() {
-    const currentScores = document.getElementsByClassName('player-current-score'); // Select the HTML elements that have class of '.player-current-score'.
-    const roundScores = document.getElementsByClassName('player-score'); // Select the HTML elements that have class of '.player-score'.
-    const dice = document.getElementsByClassName('dice')[0]; // Select the HTML img element that shows the dice.
 
     //Reset all scores.
     for (let i = 0; i < 2; i++) {
@@ -29,6 +31,7 @@ function resetGame() {
 
     //Hide the dice.
     dice.setAttribute('style','display:none;');
+    imgHidden = true; // Indicate dice is hidden.
 }
 
 //Reset the game.
@@ -37,7 +40,16 @@ resetGame();
 /* Generate random dice roll.
  */
 function generateDice() {
-    const randomDiceNumber = Math.floor(Math.random() * 7); // Generate random number.
+    const randomDiceNumber = Math.floor((Math.random() * 6) + 1); // Generate random number between 1 and 6.
+    const srcPath = 'dice-' + randomDiceNumber + '.png'; // Create path for image source.
+
+    if (imgHidden) { // If dice is hidden.
+        dice.setAttribute('style', 'display:block;');// Show the dice.
+        imgHidden = false; // Indicate dice is shown.
+    }
+
+    dice.setAttribute('src', srcPath);
+
 }
 
 /* Add click event listener on button with class 'btn-roll'.
